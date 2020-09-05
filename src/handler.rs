b4d716b08c22841e7d188a9ac62ab2b3f2662165
rimutaka <max@onebro.me>
2020-09-05 08:31:53 +0000

    Added rust-embed, split prod/debug compilation

--- src/handler.rs
+++ src/handler.rs
@@ -1,5 +1,6 @@
 use crate::{html, Error};
 use lambda::Context;
+use rust_embed::RustEmbed;
 use serde::Serialize;
 use serde_json::Value;
 use std::collections::HashMap;
@@ -16,6 +17,10 @@ struct ApiGatewayResponse {
     body: String,
 }
 
+#[derive(RustEmbed)]
+#[folder = "templates"]
+struct Asset;
+
 //pub(crate) async fn my_handler(event: Value, _ctx: Context) -> Result<Value, Error> {
 pub(crate) async fn my_handler(event: Value, _ctx: Context) -> Result<Value, Error> {
     //info!("Event: {}", event);
@@ -26,13 +31,7 @@ pub(crate) async fn my_handler(event: Value, _ctx: Context) -> Result<Value, Err
 
     let es_url = std::env::var("STACK_MUNCHER_ES_URL").expect("Missing STACK_MUNCHER_ES_URL");
 
-    let tera = match Tera::new("templates/**/*.html") {
-        Ok(t) => t,
-        Err(e) => {
-            println!("Parsing error(s): {}", e);
-            panic!();
-        }
-    };
+    let tera = tera_init()?;
 
     // do something useful here
     let html = html::html(&tera, es_url, raw_path.clone())
@@ -64,4 +63,23 @@ fn gw_response(body: String, status_code: i32) -> Result<Value, Error> {
     };
 
     Ok(serde_json::to_value(resp).expect("Failed to serialize response"))
+}
+
+/// Init Tera instance and load all HTML templates either from the file system
+/// (debug) or the binary (release).
+fn tera_init() -> Result<Tera, Error> {
+    let mut tera = Tera::default();
+
+    // loads the files from the fs or embedded strings
+    // see https://github.com/pyros2097/rust-embed
+    for file in Asset::iter() {
+        let file: &str = &file;
+        let content = Asset::get(file).expect("Cannot de-asset HTML");
+        let content = std::str::from_utf8(content.as_ref()).expect("Cannot convert HTML for str");
+
+        tera.add_raw_template(file, content)
+            .expect("Cannot add raw template");
+    }
+
+    Ok(tera)
 }
